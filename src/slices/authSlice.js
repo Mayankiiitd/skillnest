5a48f058a6a4d480c1bd56ff7c8f738a5b1d93ba
Mayankiiitd <gautammayank292@gmail.com>
2025-06-22 12:13:31 +0530

    commit changes

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,38 +1,9 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   signupData: null,
-//   loading: false,
-//   token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
-// };
-
-// const authSlice = createSlice({
-//   name: "auth",
-//   initialState: initialState,
-//   reducers: {
-//     setSignupData(state, value) {
-//       state.signupData = value.payload;
-//     },
-//     setLoading(state, value) {
-//       state.loading = value.payload;
-//     },
-//     setToken(state, value) {
-//       state.token = value.payload;
-//     },
-//   },
-// });
-
-// export const { setSignupData, setLoading, setToken } = authSlice.actions;
-
-// export default authSlice.reducer;
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   signupData: null,
   loading: false,
-  // ✅ Fixed: Don't parse the token, it's already a string
-  token: localStorage.getItem("token") || null,
+  token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
 };
 
 const authSlice = createSlice({
