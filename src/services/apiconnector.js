e0187b5e2c8370eccaee3bcf63f5cf586452a39e
Mayankiiitd <gautammayank292@gmail.com>
2025-07-16 14:54:18 +0530

    handle error

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,13 +1,42 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({});
+
+// export const apiConnector = (method, url, bodyData, headers, params) => {
+//   return axiosInstance({
+//     method: `${method}`,
+//     url: `${url}`,
+//     data: bodyData ? bodyData : null,
+//     headers: headers ? headers : null,
+//     params: params ? params : null,
+//   });
+// };
+
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({});
 
-export const apiConnector = (method, url, bodyData, headers, params) => {
+// Automatically attach token to every request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const apiConnector = (method, url, bodyData = null, headers = {}, params = null) => {
   return axiosInstance({
-    method: `${method}`,
-    url: `${url}`,
-    data: bodyData ? bodyData : null,
-    headers: headers ? headers : null,
-    params: params ? params : null,
+    method,
+    url,
+    data: bodyData,
+    headers,
+    params,
   });
 };
