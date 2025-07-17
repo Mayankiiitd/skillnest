5d65fb70d76fe329b563a17e59f79ca6b3c475ff
Mayankiiitd <gautammayank292@gmail.com>
2025-07-17 17:07:36 +0530

    commit changes

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,42 +1,13 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({});
-
-// export const apiConnector = (method, url, bodyData, headers, params) => {
-//   return axiosInstance({
-//     method: `${method}`,
-//     url: `${url}`,
-//     data: bodyData ? bodyData : null,
-//     headers: headers ? headers : null,
-//     params: params ? params : null,
-//   });
-// };
-
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({});
 
-// Automatically attach token to every request
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = JSON.parse(localStorage.getItem("token"));
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-export const apiConnector = (method, url, bodyData = null, headers = {}, params = null) => {
+export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
-    method,
-    url,
-    data: bodyData,
-    headers,
-    params,
+    method: `${method}`,
+    url: `${url}`,
+    data: bodyData ? bodyData : null,
+    headers: headers ? headers : null,
+    params: params ? params : null,
   });
 };
--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,61 +1,23 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   signupData: null,
-//   loading: false,
-//   token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
-// };
-
-// const authSlice = createSlice({
-//   name: "auth",
-//   initialState: initialState,
-//   reducers: {
-//     setSignupData(state, value) {
-//       state.signupData = value.payload;
-//     },
-//     setLoading(state, value) {
-//       state.loading = value.payload;
-//     },
-//     setToken(state, value) {
-//       state.token = value.payload;
-//     },
-//   },
-// });
-
-// export const { setSignupData, setLoading, setToken } = authSlice.actions;
-
-// export default authSlice.reducer;
-
-
 import { createSlice } from "@reduxjs/toolkit";
-
-// Get initial token from localStorage
-const storedToken = localStorage.getItem("token");
 
 const initialState = {
   signupData: null,
   loading: false,
-  token: storedToken ? JSON.parse(storedToken) : null,
+  token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
 };
 
 const authSlice = createSlice({
   name: "auth",
-  initialState,
+  initialState: initialState,
   reducers: {
-    setSignupData(state, action) {
-      state.signupData = action.payload;
+    setSignupData(state, value) {
+      state.signupData = value.payload;
     },
-    setLoading(state, action) {
-      state.loading = action.payload;
+    setLoading(state, value) {
+      state.loading = value.payload;
     },
-    setToken(state, action) {
-      state.token = action.payload;
-
-      if (action.payload) {
-        localStorage.setItem("token", JSON.stringify(action.payload));
-      } else {
-        localStorage.removeItem("token");
-      }
+    setToken(state, value) {
+      state.token = value.payload;
     },
   },
 });
