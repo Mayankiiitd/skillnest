83bb823cf32b9c1800dd8eaa73543625546671ad
Mayankiiitd <gautammayank292@gmail.com>
2025-12-14 10:59:21 +0530

    fixed Common folder case sensitivity

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -14,7 +14,7 @@ import {
   setEditCourse,
   setStep,
 } from "../../../../../slices/courseSlice"
-import IconBtn from "../../../../Common_temp/IconBtn"
+import IconBtn from "../../../../Common/IconBtn"
 import NestedView from "./NestedView"
 
 export default function CourseBuilderForm() {
--- src/components/core/Dashboard/Settings/EditProfile.jsx
+++ src/components/core/Dashboard/Settings/EditProfile.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
 import { updateProfile } from "../../../../services/operations/SettingsAPI"
-import IconBtn from "../../../Common_temp/IconBtn"
+import IconBtn from "../../../Common/IconBtn"
 
 const genders = ["Male", "Female", "Non-Binary", "Prefer not to say", "Other"]
 
